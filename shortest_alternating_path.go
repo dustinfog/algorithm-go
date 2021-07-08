d30154f02ec8c7847bf09c3ed1057fdb426be2d8
zhandong.pan <zhandong.pan@kingsgroupgames.com>
2021-07-08 15:53:37 +0800

    https://leetcode-cn.com/problems/shortest-path-with-alternating-colors/

--- shortest_alternating_path.go
+++ shortest_alternating_path.go
@@ -11,7 +11,7 @@ type node struct {
 	len   int
 }
 
-func shortestAlternatingPaths(n int, red_edges [][]int, blue_edges [][]int) []int {
+func shortestAlternatingPaths(n int, redEdges [][]int, blueEdges [][]int) []int {
 	ret := make([]int, n)
 	ret[0] = 0
 	for i := 1; i < n; i++ {
@@ -19,15 +19,15 @@ func shortestAlternatingPaths(n int, red_edges [][]int, blue_edges [][]int) []in
 	}
 
 	var edges [100]out
-	for _, edge := range red_edges {
+	for _, edge := range redEdges {
 		edges[edge[0]].red = append(edges[edge[0]].red, edge[1])
 	}
 
-	for _, edge := range blue_edges {
+	for _, edge := range blueEdges {
 		edges[edge[0]].blue = append(edges[edge[0]].blue, edge[1])
 	}
 
-	queue := []*node{}
+	queue := make([]*node, 0, len(edges[0].red)+len(edges[0].blue))
 	scatterSeek(&queue, &edges[0].red, ret, 1, true)
 	scatterSeek(&queue, &edges[0].blue, ret, 1, false)
 
@@ -45,8 +45,8 @@ func shortestAlternatingPaths(n int, red_edges [][]int, blue_edges [][]int) []in
 	return ret
 }
 
-func scatterSeek(queue *[]*node, neigbors *[]int, ret []int, l int, red bool) {
-	for _, next := range *neigbors {
+func scatterSeek(queue *[]*node, neighbors *[]int, ret []int, l int, red bool) {
+	for _, next := range *neighbors {
 		if ret[next] == -1 {
 			ret[next] = l
 		}
@@ -58,7 +58,7 @@ func scatterSeek(queue *[]*node, neigbors *[]int, ret []int, l int, red bool) {
 		})
 	}
 
-	*neigbors = nil
+	*neighbors = nil
 }
 
 //https://leetcode-cn.com/problems/shortest-path-with-alternating-colors/
